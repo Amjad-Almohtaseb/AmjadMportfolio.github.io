4ac728f2f1bf81b021dc5cdca6730af118db60a5
Amjad-Almohtaseb <amjad.almohtaseb94@gmail.com>
2021-09-08 15:24:48 +0300

    edit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function Projects() {
-    return (
-        <center id="project">
+  return (
+    <center id="project">
       <div className="card mb-3" style={{ maxWidth: "98%" }}>
         <div className="row no-gutters">
           <div className="col-md-4"></div>
 
           <div className="card-body">
             <br />
-           
+
             <p className="card-title">PROJECTS</p>
             <br />
-            <br />
+            <h3>Guide Book</h3>
             <p className="card-text fsd">
-              {/* <h4>Coding Languages:</h4> */}
+              Guide Book is created to help you to find and book your tour guide
+              for your next trip, it was built using ReactJS and Tailwind for
+              the front-end, while for the back-end we used Express and MongoDB
+              as the database
             </p>
             <br />
             <p className="card-text about"></p>
-           
           </div>
         </div>
       </div>
       ‏
     </center>
-    )
+  );
 }
 
-export default Projects
+export default Projects;
